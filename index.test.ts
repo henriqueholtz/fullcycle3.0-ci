884dc11d44c7ae745257191a5a5eecc0951dcde1
henriqueholtz <henrique_holtz@hotmail.com>
2022-04-29 03:59:47 -0300

    ci: add sonar-project.properties and workflow to github actions with test, install and sonarcloud scan

--- index.test.ts
+++ index.test.ts
@@ -1,7 +1,6 @@
 const summer = require('./sum');
-// const less = require('./less');
 
-test('Test sum function: (15+50) is equals to 65', () => {
+test('Test sum function:', () => {
   expect(summer(1, 2)).toBe(3);
   expect(summer(15, 50)).toBe(65);
 });
